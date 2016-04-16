e80bc8077bd31c09067bfbe1fb28a64c263f585e
Leandro Moreira <leandrodmoreira@gmail.com>
2016-04-15 23:06:56 -0300

    teste

---
+++ app/src/main/java/br/com/queijocombits/proejaappv5/Locate.java
@@ -0,0 +1,24 @@
+package br.com.queijocombits.proejaappv5;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Locate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_locate);
+    }
+
+    public void gotoFind(View view)
+    {
+        int number = 54;
+        Intent find = new Intent(this, find.class);
+        find.putExtra("Numero",number);
+        startActivity(find);
+    }
+
+}
